fca12d906c21f4ee441d11f29a713886bcd3eeb0
ihsankurt17 <102886451+ihsankurt17@users.noreply.github.com>
2022-04-07 23:06:22 +0200

    initial commit1

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -2,5 +2,7 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println(" Git from Hello");
          System.out.println(" Git from Hello1");
+        System.out.println(" Git from Hello2");
+        System.out.println(" Git from Hello13");
     }
 }
